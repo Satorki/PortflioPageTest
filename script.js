2d2f3509e9a57bd390b0e543af7369e55463d41d
Satorki <manzajii@gmai.com>
2023-07-14 16:32:34 +0200

    Corrected arrow and smallen umiejetnosci icons

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const doGory = (id) => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+const doGory = () => {
+    window.scrollTo(
+        { top: 0, behavior: 'smooth' }
+        );
+}
+
+let strzalka = document.getElementById("strzalka-klik");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        strzalka.style.display = "block";
+    } else {
+        strzalka.style.display = "none";
+    }
 }
